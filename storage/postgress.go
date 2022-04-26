036ecf7c62c2b4e85aa52720038f2d7aaaa08543
Bilal <s9iper1@gmail.com>
2022-04-26 18:31:34 +0500

    comment unused function newConnection

--- storage/postgress.go
+++ storage/postgress.go
@@ -1,11 +1,5 @@
 package storage
 
-import (
-	"fmt"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
-)
-
 type Config struct {
 	Host     string
 	Port     string
@@ -15,12 +9,12 @@ type Config struct {
 	SSLMode  string
 }
 
-func NewConnection(c *Config) (*gorm.DB, error) {
-	connectionInfo := fmt.Sprintf("host=%s port=%s user=%spassword=%s dbname=%s sslmode=%s",
-		c.Host, c.Port, c.User, c.Password, c.DBName, c.SSLMode)
-	db, err := gorm.Open(postgres.Open(connectionInfo), &gorm.Config{})
-	if err != nil {
-		return db, nil
-	}
-	return db, nil
-}
+//func NewConnection(c *Config) (*gorm.DB, error) {
+//	connectionInfo := fmt.Sprintf("host=%s port=%s user=%spassword=%s dbname=%s sslmode=%s",
+//		c.Host, c.Port, c.User, c.Password, c.DBName, c.SSLMode)
+//	db, err := gorm.Open(postgres.Open(connectionInfo), &gorm.Config{})
+//	if err != nil {
+//		return db, nil
+//	}
+//	return db, nil
+//}
